fc34a38c0d309824a192b9f17b9dffde7a9c42ca
Gordon Wong <gwong001@gmail.com>
2015-02-27 22:07:12 -0800

    Remove font-awesome dependency

--- index.js
+++ index.js
@@ -2,25 +2,5 @@
 'use strict';
 
 module.exports = {
-    name: 'ember-cli-notifications',
-    included: function(app) {
-        this._super.included(app);
-
-        app.import(app.bowerDirectory + '/font-awesome/fonts/fontawesome-webfont.eot', {
-            destDir: 'fonts'
-        });
-        app.import(app.bowerDirectory + '/font-awesome/fonts/fontawesome-webfont.svg', {
-            destDir: 'fonts'
-        });
-        app.import(app.bowerDirectory + '/font-awesome/fonts/fontawesome-webfont.ttf', {
-            destDir: 'fonts'
-        });
-        app.import(app.bowerDirectory + '/font-awesome/fonts/fontawesome-webfont.woff', {
-            destDir: 'fonts'
-        });
-        app.import(app.bowerDirectory + '/font-awesome/fonts/fontawesome-webfont.woff2', {
-            destDir: 'fonts'
-        });
-        app.import(app.bowerDirectory + '/font-awesome/css/font-awesome.css');
-    }
+    name: 'ember-cli-notifications'
 };
